37132b40d21f9271ccbe869e163fedb50335bb8c
Kamille Norris <kamionorato@gmail.com>
2021-03-18 11:25:50 -0400

    Test inaccessible TSX and MDX

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,13 @@ const Home = ({ ...props }) => (
   <Layout {...props}>
     <h1>Welcome!</h1>
     <p>This website is currently under development</p>
+
+    <form>
+      <div>My Form Label</div>
+      <input type="text" />
+
+      <button>Submit</button>
+    </form>
   </Layout>
 );
 
