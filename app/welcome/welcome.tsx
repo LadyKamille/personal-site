fcf8a1370b65aedd325f49c7d191a60a4e9aea9b
Kamille Norris <kamionorato@gmail.com>
2026-01-02 15:19:54 -0500

    Little bit of cleanup

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -5,21 +5,27 @@ import linkedIn from './linkedin.svg';
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex flex-col sm:flex-row items-center gap-16 min-h-0">
-        <div className="max-w-[100vw] p-4">
+    <main className="h-screen overflow-y-scroll snap-y snap-mandatory md:snap-none">
+      <section className="h-screen snap-start flex flex-col sm:flex-row items-center justify-center gap-16">
+        <div className="p-4">
           <img
             src={headshot}
             alt="Kamille Norris"
             className="block w-75 rounded-full"
           />
         </div>
-        <div className="space-y-6 px-4 text-center sm:text-left">
+        <div className="space-y-6 px-4 text-center sm:text-left lg:max-w-150">
           <h1 className="text-3xl">Hi, I'm Kamille Norris</h1>
           <h2 className="text-xl">
             Staff Software Engineer /{' '}
             <span className="dark:text-rose-300">Angular Expert</span>
           </h2>
+
+          <p>
+            <span className="italic">Technically</span> fullstack but primarily
+            frontend focused engineer with over 10 years of experience.
+          </p>
+
           <ul className="flex justify-center sm:justify-start">
             {resources.map(({ href, text, icon }) => (
               <li key={href} className="p-3 first-of-type:pl-0">
@@ -39,7 +45,7 @@ export function Welcome() {
             ))}
           </ul>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
